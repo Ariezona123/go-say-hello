4bb0d2a3614c65e6b7c921cecfc4d1360d82cb6d
unknown <arieafriyanto1804@gmail.com>
2022-04-04 21:33:19 +0700

    first commit

---
+++ say_hello.go
@@ -0,0 +1,13 @@
+package go_say_hello
+
+func SayHello(nama string) string {
+	return "Hello " + nama
+}
+
+func Kali(a int, b int) int {
+	return a * b
+}
+
+func Tambah(x int, y int) int {
+	return x + y
+}
